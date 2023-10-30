a0d7048cf9fa711c582ca069e6c03ccc06133fee
Sam Becker <sam@sambecker.com>
2023-10-30 09:37:02 -0500

    Fix film simulation in light mode

--- src/app/(static)/film/animate/page.tsx
+++ src/app/(static)/film/animate/page.tsx
@@ -39,7 +39,9 @@ export default function FilmPage() {
             </div>
             <div className={cc(
               'absolute top-0 left-[-2px] right-0 bottom-0',
-              'bg-gradient-to-t from-black to-[rgba(0,0,0,0.5)]',
+              'bg-gradient-to-t',
+              'from-white to-[rgba(255,255,255,0.5)]',
+              'dark:from-black dark:to-[rgba(0,0,0,0.5)]',
             )} />
           </div>
         </div>
