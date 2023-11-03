06eed08256530f7be090211b29fc2ec8cfa53e00
Sam Becker <sam@sambecker.com>
2023-11-02 19:05:10 -0500

    Tweak badge styles

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -12,12 +12,13 @@ export default function Badge({
   const baseStyles = () => {
     switch (type) {
     case 'primary': return cc(
-      'px-2 py-0.5 rounded-md bg-gray-100 dark:bg-gray-900',
-      'border border-gray-200 dark:border-gray-800'
+      'px-1.5 py-[0.3rem] leading-none rounded-md',
+      'bg-gray-100/80 dark:bg-gray-900/80',
+      'border border-gray-200/60 dark:border-gray-800/75'
     );
     case 'secondary': return cc(
-      'px-1 py-1 leading-none rounded-md',
-      'bg-gray-100 dark:bg-gray-800/75',
+      'px-1.5 py-1 leading-none rounded-[0.25rem]',
+      'bg-gray-100 dark:bg-gray-800/60',
       'text-medium',
       'font-medium text-[0.7rem]',
     );
