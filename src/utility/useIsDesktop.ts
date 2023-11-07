f45d555989d5c74ba9c00b4fce5f6aeeecb6eab1
Sam Becker <sam@sambecker.com>
2023-11-06 23:04:42 -0600

    Store useIsDesktop hook as variable first

--- src/utility/useIsDesktop.ts
+++ src/utility/useIsDesktop.ts
@@ -5,5 +5,6 @@ import tailwindConfig from '@/../tailwind.config';
 const screens = resolveConfig(tailwindConfig).theme?.screens as any;
 
 export default function useIsDesktop() {
-  return useMediaQuery({ query: `(min-width: ${screens.md})` });
+  const isDesktop = useMediaQuery({ query: `(min-width: ${screens.md})` });
+  return isDesktop;
 };
