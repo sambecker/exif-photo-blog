f431470e7bffd4cc94501cc1bb105b370db4b513
Sam Becker <sam@sambecker.com>
2023-11-07 00:13:48 -0600

    Create custom media query hook

--- src/utility/useIsDesktop.ts
+++ src/utility/useIsDesktop.ts
@@ -1,10 +1,23 @@
-import { useMediaQuery } from 'react-responsive';
 import resolveConfig from 'tailwindcss/resolveConfig';
 import tailwindConfig from '@/../tailwind.config';
+import { useLayoutEffect, useState } from 'react';
 
 const screens = resolveConfig(tailwindConfig).theme?.screens as any;
 
 export default function useIsDesktop() {
-  const isDesktop = useMediaQuery({ query: `(min-width: ${screens.md})` });
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useLayoutEffect(() => {
+    if (typeof window !== 'undefined') {
+      const mql = window.matchMedia(`(min-width: ${screens.md})`);
+      setIsDesktop(mql.matches);
+      const eventHandler = (event: MediaQueryListEvent) => {
+        setIsDesktop(event.matches);
+      };
+      mql.addEventListener('change', eventHandler);
+      return () => mql.removeEventListener('change', eventHandler);
+    }
+  }, []);
+
   return isDesktop;
 };
