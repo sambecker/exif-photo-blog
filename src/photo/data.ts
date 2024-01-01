4c3c2a73ef417748b847e5fd7337783ad9a1cb07
Sam Becker <sam@sambecker.com>
2023-12-31 21:44:40 -0500

    Prevent empty favs tag

--- src/photo/data.ts
+++ src/photo/data.ts
@@ -5,13 +5,13 @@ import {
   getUniqueTagsCached,
 } from '@/cache';
 import { SHOW_FILM_SIMULATIONS } from '@/site/config';
-import { TAG_FAVS, Tags } from '@/tag';
+import { TAG_FAVS } from '@/tag';
 
 export const getPhotoSidebarDataCached = () => [
   getPhotosCountCached(),
   getUniqueTagsCached().then(tags =>
-    ([tags.find(({ tag }) => tag === TAG_FAVS) ?? []] as Tags)
-      .concat(tags.filter(({ tag }) => tag !== TAG_FAVS))),
+    tags.filter(({ tag }) => tag === TAG_FAVS).concat(
+      tags.filter(({ tag }) => tag !== TAG_FAVS))),
   getUniqueCamerasCached(),
   SHOW_FILM_SIMULATIONS ? getUniqueFilmSimulationsCached() : [],
 ] as const;
