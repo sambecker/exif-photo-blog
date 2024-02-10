8151a4f1cd67f6b509ce6f4e27907c1df4540a29
Sam Becker <sam@sambecker.com>
2024-02-09 20:56:21 -0600

    Simplify AdminPhotoMenu types

--- src/admin/AdminPhotoMenuClient.tsx
+++ src/admin/AdminPhotoMenuClient.tsx
@@ -4,15 +4,12 @@ import { ComponentProps } from 'react';
 import { pathForAdminPhotoEdit } from '@/site/paths';
 import MoreMenu from '../components/MoreMenu';
 
-export interface AdminPhotoMenuClientProps
-  extends Omit<ComponentProps<typeof MoreMenu>, 'items'> {
-  photoId: string
-}
-
 export default function AdminPhotoMenuClient({
   photoId,
   ...props
-}: AdminPhotoMenuClientProps) {
+}: Omit<ComponentProps<typeof MoreMenu>, 'items'> & {
+  photoId: string
+}) {
   return (
     <MoreMenu {...{
       items: [{ href: pathForAdminPhotoEdit(photoId), label: 'Edit Photo' }],
