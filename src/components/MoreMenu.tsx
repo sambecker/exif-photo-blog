81ba7228b5ed691bbfd0a001fc4c7758561c1cf7
Sam Becker <sam@sambecker.com>
2024-02-10 23:50:03 -0600

    Make admin menu text consistent

--- src/components/MoreMenu.tsx
+++ src/components/MoreMenu.tsx
@@ -23,7 +23,7 @@ export default function MoreMenu({
   const itemClass = clsx(
     'block w-full',
     'border-none min-h-0 bg-transparent',
-    'text-left',
+    'text-left text-main',
     'px-3 py-1.5 rounded-[3px]',
     'hover:text-main',
     'hover:bg-gray-50 active:bg-gray-100',
@@ -59,7 +59,7 @@ export default function MoreMenu({
         <Menu.Items className={clsx(
           'absolute top-6',
           'min-w-[9rem]',
-          'text-sm text-left',
+          'text-left',
           'md:right-1',
           'p-1 rounded-md border',
           'bg-content outline-none',
