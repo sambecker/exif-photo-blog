a19b7df9c3c1a7b6a10fc8bdcf33700e98c9e740
Sam Becker <sam@sambecker.com>
2024-03-12 17:19:43 -0500

    Fix Tag type

--- src/photo/UploadPageClient.tsx
+++ src/photo/UploadPageClient.tsx
@@ -3,9 +3,9 @@
 import AdminChildPage from '@/components/AdminChildPage';
 import { PATH_ADMIN_UPLOADS } from '@/site/paths';
 import { PhotoFormData } from './form';
-import { Tags } from '@/tag';
 import PhotoForm from './form/PhotoForm';
 import { useState } from 'react';
+import { TagsWithMeta } from '@/tag';
 
 export default function UploadPageClient({
   blobId,
@@ -14,7 +14,7 @@ export default function UploadPageClient({
 }: {
   blobId?: string
   photoFormExif: Partial<PhotoFormData>
-  uniqueTags: Tags
+  uniqueTags: TagsWithMeta
 }) {
   const [pending, setIsPending] = useState(false);
 
