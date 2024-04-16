9d1838e87bae40a974f19be33136cf72d3c1c684
Sam Becker <sam@sambecker.com>
2024-04-16 16:59:37 -0500

    Move all functions to nodejs runtime

--- src/app/p/[photoId]/image/route.tsx
+++ src/app/p/[photoId]/image/route.tsx
@@ -5,8 +5,6 @@ import { getIBMPlexMonoMedium } from '@/site/font';
 import { ImageResponse } from 'next/og';
 import { getImageResponseCacheControlHeaders } from '@/image-response/cache';
 
-export const runtime = 'edge';
-
 export async function GET(
   _: Request,
   context: { params: { photoId: string } },
