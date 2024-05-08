69ae33928d12f52c8e8fe928416d3280a680f3af
Dhanuka Perera <dhanukashehanperera@gmail.com>
2024-05-08 20:51:08 +0100

    Revert "feat: add cookie bot"
    This reverts commit 3f9802922b6a71b84476abec7d43519a0362765b.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,6 @@ import GoogleAnalytics from '@/components/GoogleAnalytics';
 
 import '../site/globals.css';
 import '../site/sonner.css';
-import CookieBot from '@/components/CookieBot';
 
 const ibmPlexMono = IBM_Plex_Mono({
   subsets: ['latin'],
@@ -78,7 +77,6 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <GoogleAnalytics />
-      <CookieBot />
       <body className={ibmPlexMono.variable}>
         <AppStateProvider>
           <SwrConfigClient>
@@ -100,7 +98,7 @@ export default function RootLayout({
             </ThemeProvider>
           </SwrConfigClient>
           <Analytics debug={false} />
-          <SpeedInsights debug={false} />
+          <SpeedInsights debug={false}  />
           <PhotoEscapeHandler />
           <ToasterWithThemes />
         </AppStateProvider>
