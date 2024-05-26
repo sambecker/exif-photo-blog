a6c6a562715dd66c566168b1aea8490aa80da037
Sam Becker <sam@sambecker.com>
2024-05-26 11:20:16 -0500

    Adjust X share url to help open in native app

--- src/utility/social.ts
+++ src/utility/social.ts
@@ -1,6 +1,6 @@
 export const generateXPostText = (path: string, text: string) => {
-  const url = new URL('https://x.com/intent/post');
-  url.searchParams.set('text', text);
+  const url = new URL('https://x.com/intent/tweet');
   url.searchParams.set('url', path);
+  url.searchParams.set('text', text);
   return url.toString();
 };
