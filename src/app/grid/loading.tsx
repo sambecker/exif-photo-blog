5134d443e05e9f5967f6bed929e7e21c8350d642
ray <nnn>
2024-06-04 09:15:15 +0700

    增加loading

---
+++ src/app/grid/loading.tsx
@@ -0,0 +1,5 @@
+//约定大于配置,如果page.tsx的return之前有await，则会显示loading
+export default function Loading() {
+  return <div>loading...</div>
+  // return <DashboardSkeleton />;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,49 +2,57 @@ import {
 	INFINITE_SCROLL_LARGE_PHOTO_INITIAL,
 	INFINITE_SCROLL_LARGE_PHOTO_MULTIPLE,
 	generateOgImageMetaForPhotos,
-  } from '@/photo';
-  import PhotosEmptyState from '@/photo/PhotosEmptyState';
-  import { Metadata } from 'next/types';
-  import PhotosLarge from '@/photo/PhotosLarge';
-  import { cache } from 'react';
-  import { getPhotos, getPhotosMeta } from '@/photo/db/query';
-  import PhotosLargeInfinite from '@/photo/PhotosLargeInfinite';
-  
-  export const dynamic = 'force-static';
-  
-  const getPhotosCached = cache(() => getPhotos({
-	limit: INFINITE_SCROLL_LARGE_PHOTO_INITIAL,
-  }));
-  
-  export async function generateMetadata(): Promise<Metadata> {
-	const photos = await getPhotosCached()
-	  .catch(() => []);
+} from '@/photo';
+import PhotosEmptyState from '@/photo/PhotosEmptyState';
+import { Metadata } from 'next/types';
+import PhotosLarge from '@/photo/PhotosLarge';
+import { cache } from 'react';
+import { getPhotos, getPhotosMeta } from '@/photo/db/query';
+import PhotosLargeInfinite from '@/photo/PhotosLargeInfinite';
+import { timeEnd } from 'console';
+
+export const dynamic = 'force-static';
+
+const getPhotosCached = cache(() =>
+	getPhotos({
+		limit: INFINITE_SCROLL_LARGE_PHOTO_INITIAL,
+	}),
+);
+
+export async function generateMetadata(): Promise<Metadata> {
+	const photos = await getPhotosCached().catch(() => []);
 	return generateOgImageMetaForPhotos(photos);
-  }
-  
-  export default async function HomePage() {
-	const [
-	  photos,
-	  photosCount,
-	] = await Promise.all([
-	  getPhotosCached()
-		.catch(() => []),
-	  getPhotosMeta()
-		.then(({ count }) => count)
-		.catch(() => 0),
+}
+
+export default async function HomePage() {
+	console.time("zidingyishijianaaaaaaaaaaaaaaa");
+	const [photos, photosCount] = await Promise.all([
+		getPhotosCached().catch(() => []),
+		getPhotosMeta()
+			.then(({ count }) => count)
+			.catch(() => 0),
 	]);
-  
-	return (
-	  photos.length > 0
-		? <div className="space-y-1">
-		  <PhotosLarge {...{ photos }} />
-		  {photosCount > photos.length &&
-			<PhotosLargeInfinite
-			  initialOffset={INFINITE_SCROLL_LARGE_PHOTO_INITIAL}
-			  itemsPerPage={INFINITE_SCROLL_LARGE_PHOTO_MULTIPLE}
-			/>}
+	console.timeEnd('zidingyishijianaaaaaaaaaaaaaaa');
+	// console.time('111111111111'); // 开始计时第一个请求
+	// const photos = await getPhotosCached().catch(() => []);
+	// console.timeEnd('111111111111'); // 结束第一个请求计时
+
+	// console.time('22222222222222'); // 开始计时第二个请求
+	// const { count: photosCount } = await getPhotosMeta().catch(() => ({
+	// 	count: 0,
+	// }));
+	// console.timeEnd('22222222222222'); // 结束第二个请求计时
+	return photos.length > 0 ? (
+		<div className="space-y-1">
+			<PhotosLarge {...{ photos }} />
+			{photosCount > photos.length && (
+				<PhotosLargeInfinite
+					initialOffset={INFINITE_SCROLL_LARGE_PHOTO_INITIAL}
+					itemsPerPage={INFINITE_SCROLL_LARGE_PHOTO_MULTIPLE}
+				/>
+			)}
 		</div>
-		: <PhotosEmptyState />
+	) : (
+		<PhotosEmptyState />
 	);
-  }
-  
+}
