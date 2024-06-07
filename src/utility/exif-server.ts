d04404582bf0c18294393ca73763da400bcb14b0
Sam Becker <sam@sambecker.com>
2024-06-07 00:45:09 -0500

    Remove piexifjs annotation

--- src/utility/exif-server.ts
+++ src/utility/exif-server.ts
@@ -6,7 +6,7 @@ export const stripGpsFromFile = async (
   const base64 = Buffer.from(fileBytes).toString('base64');
   const base64Url = `data:image/jpeg;base64,${base64}`;
 
-  const exifObject = PiExif.load(base64Url) as Record<string, any>;
+  const exifObject = PiExif.load(base64Url);
   delete exifObject.GPS;
   const exifDataWithoutGps = PiExif.dump(exifObject);
 
