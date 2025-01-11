39035bf188fb506d60ba95ccd385bb0f3bc632e9
Sam Becker <sam@sambecker.com>
2025-01-11 17:51:02 -0600

    Remove all share pages/layouts

--- src/app/film/[simulation]/[photoId]/page.tsx
+++ src/app/film/[simulation]/[photoId]/page.tsx
@@ -1,3 +1,89 @@
-export default function Page() {
-  return null;
+import {
+  RELATED_GRID_PHOTOS_TO_SHOW,
+  descriptionForPhoto,
+  titleForPhoto,
+} from '@/photo';
+import { Metadata } from 'next/types';
+import { redirect } from 'next/navigation';
+import {
+  PATH_ROOT,
+  absolutePathForPhoto,
+  absolutePathForPhotoImage,
+} from '@/site/paths';
+import PhotoDetailPage from '@/photo/PhotoDetailPage';
+import { FilmSimulation } from '@/simulation';
+import {
+  getPhotosMetaCached,
+  getPhotosNearIdCached,
+} from '@/photo/cache';
+import { cache } from 'react';
+
+const getPhotosNearIdCachedCached = cache((
+  photoId: string,
+  simulation: FilmSimulation,
+) =>
+  getPhotosNearIdCached(
+    photoId,
+    { simulation, limit: RELATED_GRID_PHOTOS_TO_SHOW + 2 },
+  ));
+
+interface PhotoFilmSimulationProps {
+  params: Promise<{ photoId: string, simulation: FilmSimulation }>
+}
+
+export async function generateMetadata({
+  params,
+}: PhotoFilmSimulationProps): Promise<Metadata> {
+  const { photoId, simulation } = await params;
+
+  const { photo } = await getPhotosNearIdCachedCached(photoId, simulation);
+
+  if (!photo) { return {}; }
+
+  const title = titleForPhoto(photo);
+  const description = descriptionForPhoto(photo);
+  const images = absolutePathForPhotoImage(photo);
+  const url = absolutePathForPhoto({ photo, simulation });
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      images,
+      description,
+      url,
+    },
+    twitter: {
+      title,
+      description,
+      images,
+      card: 'summary_large_image',
+    },
+  };
+}
+
+export default async function PhotoFilmSimulationPage({
+  params,
+}: PhotoFilmSimulationProps) {
+  const { photoId, simulation } = await params;
+
+  const { photo, photos, photosGrid, indexNumber } =
+    await getPhotosNearIdCachedCached(photoId, simulation);
+
+  if (!photo) { redirect(PATH_ROOT); }
+
+  const { count, dateRange } = await getPhotosMetaCached({ simulation });
+
+  return (
+    <PhotoDetailPage {...{
+      photo,
+      photos,
+      photosGrid,
+      simulation,
+      indexNumber,
+      count,
+      dateRange,
+    }} />
+  );
 }
--- src/app/p/[photoId]/page.tsx
+++ src/app/p/[photoId]/page.tsx
@@ -1,3 +1,82 @@
-export default function Page() {
-  return null;
+import {
+  RELATED_GRID_PHOTOS_TO_SHOW,
+  descriptionForPhoto,
+  titleForPhoto,
+} from '@/photo';
+import { Metadata } from 'next/types';
+import { redirect } from 'next/navigation';
+import {
+  PATH_ROOT,
+  absolutePathForPhoto,
+  absolutePathForPhotoImage,
+} from '@/site/paths';
+import PhotoDetailPage from '@/photo/PhotoDetailPage';
+import { getPhotosNearIdCached } from '@/photo/cache';
+import { IS_PRODUCTION, STATICALLY_OPTIMIZED_PAGES } from '@/site/config';
+import { getPhotoIds } from '@/photo/db/query';
+import { GENERATE_STATIC_PARAMS_LIMIT } from '@/photo/db';
+import { cache } from 'react';
+
+export const maxDuration = 60;
+
+const getPhotosNearIdCachedCached = cache((photoId: string) =>
+  getPhotosNearIdCached(photoId, { limit: RELATED_GRID_PHOTOS_TO_SHOW + 2 }));
+
+export let generateStaticParams:
+  (() => Promise<{ photoId: string }[]>) | undefined = undefined;
+
+if (STATICALLY_OPTIMIZED_PAGES && IS_PRODUCTION) {
+  generateStaticParams = async () => {
+    const photos = await getPhotoIds({ limit: GENERATE_STATIC_PARAMS_LIMIT });
+    return photos.map(photoId => ({ photoId }));
+  };
+}
+
+interface PhotoProps {
+  params: Promise<{ photoId: string }>
+}
+
+export async function generateMetadata({
+  params,
+}:PhotoProps): Promise<Metadata> {
+  const { photoId } = await params;
+  const { photo } = await getPhotosNearIdCachedCached(photoId);
+
+  if (!photo) { return {}; }
+
+  const title = titleForPhoto(photo);
+  const description = descriptionForPhoto(photo);
+  const images = absolutePathForPhotoImage(photo);
+  const url = absolutePathForPhoto({ photo });
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      images,
+      description,
+      url,
+    },
+    twitter: {
+      title,
+      description,
+      images,
+      card: 'summary_large_image',
+    },
+  };
+}
+
+export default async function PhotoPage({
+  params,
+}: PhotoProps) {
+  const { photoId } = await params;
+  const { photo, photos, photosGrid } =
+    await getPhotosNearIdCachedCached(photoId);
+
+  if (!photo) { redirect(PATH_ROOT); }
+
+  return (
+    <PhotoDetailPage {...{ photo, photos, photosGrid }} />
+  );
 }
