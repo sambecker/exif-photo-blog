d132e85cedb007c401f1d89f41adf33f5933f167
Sam Becker <sam@sambecker.com>
2025-02-04 19:02:07 -0600

    Adjust tooltip proportions

--- src/components/primitives/MenuSurface.tsx
+++ src/components/primitives/MenuSurface.tsx
@@ -15,7 +15,7 @@ export default function MenuSurface({
       ref={ref}
       className={clsx(
         'component-surface',
-        'px-2 pt-1.5 pb-2 max-w-[14rem]',
+        'px-2 py-1.5 max-w-[14rem]',
         'shadow-sm',
         'text-[0.8rem] leading-tight',
         'text-balance text-center',
