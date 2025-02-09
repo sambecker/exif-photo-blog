3c04ca840f9a5747f7b0090bba27fb1c92e5dd16
Sam Becker <sam@sambecker.com>
2025-02-09 09:54:17 -0600

    Stop checking for window in useEffect

--- src/utility/useIsDesktop.ts
+++ src/utility/useIsDesktop.ts
@@ -4,18 +4,16 @@ export default function useIsDesktop() {
   const [isDesktop, setIsDesktop] = useState<boolean>();
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const breakpointMd = getComputedStyle(document.body)
-        .getPropertyValue('--breakpoint-md');
-      const mql = window.matchMedia(`(min-width: ${breakpointMd})`);
-      setIsDesktop(mql.matches);
+    const breakpointMd = getComputedStyle(document.body)
+      .getPropertyValue('--breakpoint-md');
+    const mql = window.matchMedia(`(min-width: ${breakpointMd})`);
+    setIsDesktop(mql.matches);
 
-      const eventHandler = (event: MediaQueryListEvent) =>
-        setIsDesktop(event.matches);
+    const eventHandler = (event: MediaQueryListEvent) =>
+      setIsDesktop(event.matches);
 
-      mql.addEventListener('change', eventHandler);
-      return () => mql.removeEventListener('change', eventHandler);
-    }
+    mql.addEventListener('change', eventHandler);
+    return () => mql.removeEventListener('change', eventHandler);
   }, []);
 
   return isDesktop;
