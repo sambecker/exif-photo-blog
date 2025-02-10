befdf2b868b419e607b6f6f252cde7338e300e5e
Sam Becker <sam@sambecker.com>
2025-02-09 23:24:17 -0600

    Add more photo library insights

--- src/app/admin/insights/page.tsx
+++ src/app/admin/insights/page.tsx
@@ -1,11 +1,23 @@
 import AdminInfoPage from '@/admin/AdminInfoPage';
 import GitHubForkStatusBadge from '@/admin/github/GitHubForkStatusBadge';
 import { dateRangeForPhotos } from '@/photo';
-import { getPhotosMetaCached } from '@/photo/cache';
+import {
+  getPhotosMeta,
+  getUniqueTags,
+  getUniqueCameras,
+} from '@/photo/db/query';
 import { IS_DEVELOPMENT, IS_VERCEL_GIT_PROVIDER_GITHUB } from '@/site/config';
 
 export default async function AdminInsightsPage() {
-  const { count, dateRange } = await getPhotosMetaCached();
+  const [
+    { count, dateRange },
+    tags,
+    cameras,
+  ]= await Promise.all([
+    getPhotosMeta(),
+    getUniqueTags(),
+    getUniqueCameras(),
+  ]);
 
   const { start, end } = dateRangeForPhotos(undefined, dateRange);
 
@@ -15,8 +27,17 @@ export default async function AdminInsightsPage() {
       <GitHubForkStatusBadge />}
   >
     <div className="flex flex-col justify-center gap-4 *:text-center">
+      <div className="font-bold uppercase text-main">
+        Photo library
+      </div>
       <div>
         {count} photos
+      </div>
+      <div>
+        {tags.length} tags
+      </div>
+      <div>
+        {cameras.length} cameras
       </div>
       <span className="text-dim uppercase">
         {start === end
