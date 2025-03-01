90d26a1fd1b4173159a3120d4e21030b489257cd
Sam Becker <sam@sambecker.com>
2025-02-28 20:56:49 -0600

    Redirect from /admin/uploads when there are no uploads

--- app/admin/uploads/page.tsx
+++ app/admin/uploads/page.tsx
@@ -2,19 +2,26 @@ import { getStorageUploadUrlsNoStore } from '@/platforms/storage/cache';
 import SiteGrid from '@/components/SiteGrid';
 import { getUniqueTagsCached } from '@/photo/cache';
 import AdminUploadsClient from '@/admin/AdminUploadsClient';
+import { redirect } from 'next/navigation';
+import { PATH_ADMIN_PHOTOS } from '@/app/paths';
 
 export const maxDuration = 60;
 
 export default async function AdminUploadsPage() {
   const urls = await getStorageUploadUrlsNoStore();
   const uniqueTags = await getUniqueTagsCached();
-  return (
-    <SiteGrid
-      contentMain={
-        <AdminUploadsClient {...{
-          urls,
-          uniqueTags,
-        }} />}
-    />
-  );
+
+  if (urls.length === 0) {
+    redirect(PATH_ADMIN_PHOTOS);
+  } else {
+    return (
+      <SiteGrid
+        contentMain={
+          <AdminUploadsClient {...{
+            urls,
+            uniqueTags,
+          }} />}
+      />
+    );
+  }
 }
