bc0216f83205ff2c1815e38ccfff4ded0ed1cdf4
Sam Becker <sam@sambecker.com>
2025-03-20 23:01:48 -0500

    Account of screen modifier 'A' in sony models

--- src/platforms/sony.ts
+++ src/platforms/sony.ts
@@ -12,14 +12,17 @@ export const formatSonyModel = (model: string) => {
     series,
     letter,
     _version,
-  ] = /^SONY (ILCE|ILME)-([0-9]*)([a-ln-z]*)M*([0-9]*)/gi.exec(model) ?? [];
+    modifier,
+  // eslint-disable-next-line max-len
+  ] = /^SONY (ILCE|ILME)-([0-9]*)([a-ln-z]*)M*([0-9]*)([a-z]*)/gi.exec(model) ?? [];
   const version = parseInt(_version ?? '0');
   const versionRomanNumber = version > 1 && version < 10
     ? ` ${convertNumberToRomanNumeral(version)}`
     : undefined;
   if (type === 'ILCE' || type === 'ILME') {
     return type === 'ILCE'
-      ? `A${series}${letter}${versionRomanNumber || version || ''}`
+      // eslint-disable-next-line max-len
+      ? `A${series}${letter}${versionRomanNumber || version || ''}${modifier || ''}`
       : `FX${series}${_version}`;
   }
   return model;
