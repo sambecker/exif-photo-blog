c959e88ebb3ebdc63d8365ef9212a6b33a19354e
Sam Becker <sam@sambecker.com>
2025-04-03 09:33:53 -0500

    Fix scroll direction detection for Safari rubber-banding

--- src/utility/useScrollDirection.ts
+++ src/utility/useScrollDirection.ts
@@ -1,14 +1,18 @@
 import { useEffect, useState, useRef } from 'react';
 
 export default function useScrollDirection() {
-  const [scrollDirection, setScrollDirection] =
-    useState<'up' | 'down'>('down');
+  const [scrollDirection, setScrollDirection] = useState<'up' | 'down'>('down');
 
   const lastScrollY = useRef(0);
 
   useEffect(() => {
     const handleScroll = () => {
-      setScrollDirection(window.scrollY > lastScrollY.current ? 'down' : 'up');
+      const pageHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      setScrollDirection((
+        window.scrollY >= lastScrollY.current ||
+        lastScrollY.current > pageHeight
+      ) ? 'down' : 'up');
       lastScrollY.current = window.scrollY;
     };
     window.addEventListener('scroll', handleScroll);
