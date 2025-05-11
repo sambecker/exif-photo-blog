99faf29107e65bc3f08930b66287472df2ac8979
João Pedro Sconetto <sconetto.joao@gmail.com>
2025-05-11 16:34:38 -0300

    feat(i18n): update pt-br and add pt-pt
    - correct trasnlations for pt-br
    - add app traslations for pt-pt
    - update index to import and include config for pt-pt
    - solves #247
    
    Signed-off-by: João Pedro Sconetto <sconetto.joao@gmail.com>

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,5 +1,6 @@
 import US_EN from './locales/us-en';
 import PT_BR from './locales/pt-br';
+import PT_PT from './locales/pt-pt';
 
 export type I18N = typeof US_EN;
 
@@ -9,8 +10,9 @@ export type I18NDeepPartial = {
 
 export const LOCALE_TEXT: Record<string, I18NDeepPartial | undefined> = {
   'pt-br': PT_BR,
+  'pt-pt': PT_PT,
 };
-  
+
 export const getTextForLocale = (
   locale = '',
 ): I18N => {
