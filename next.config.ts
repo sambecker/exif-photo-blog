e0a901ff59b0f3e9a053c36aff53d333215e65c9
Sam Becker <sam@sambecker.com>
2025-06-01 15:13:52 -0500

    Tweak webpack config

--- next.config.ts
+++ next.config.ts
@@ -56,11 +56,9 @@ const nextConfig: NextConfig = {
     },
   },
   webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      // eslint-disable-next-line max-len
-      '@/i18n/date-fns-locale-alias': path.resolve(__dirname, `src/i18n/locales/${process.env.NEXT_PUBLIC_LOCALE ?? 'en-us'}`),
-    };
+    config.cache = false;
+    // eslint-disable-next-line max-len
+    config.resolve.alias['@/i18n/date-fns-locale-alias'] = path.resolve(__dirname, `src/i18n/locales/${process.env.NEXT_PUBLIC_LOCALE ?? 'en-us'}`);
     console.log(config.resolve.alias);
     return config;
   },
