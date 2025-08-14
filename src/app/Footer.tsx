08656f1db34f3431f84e9228515508081eb4c38a
Simon Deeley <simondeeley@users.noreply.github.com>
2025-08-14 15:06:23 +0100

    Remove admin login link from footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -3,11 +3,10 @@
 import { clsx } from 'clsx/lite';
 import AppGrid from '../components/AppGrid';
 import ThemeSwitcher from '@/app/ThemeSwitcher';
-import Link from 'next/link';
 import { SHOW_REPO_LINK } from '@/app/config';
 import RepoLink from '../components/RepoLink';
 import { usePathname } from 'next/navigation';
-import { PATH_ADMIN_PHOTOS, isPathAdmin, isPathSignIn } from './path';
+import { isPathAdmin, isPathSignIn } from './path';
 import SubmitButtonWithStatus from '@/components/SubmitButtonWithStatus';
 import { signOutAction } from '@/auth/actions';
 import AnimateItems from '@/components/AnimateItems';
@@ -62,9 +61,7 @@ export default function Footer() {
                     ? <Spinner size={16} className="translate-y-[2px]" />
                     : SHOW_REPO_LINK
                       ? <RepoLink />
-                      : <Link href={PATH_ADMIN_PHOTOS}>
-                        {appText.nav.admin}
-                      </Link>}
+                      : ''}
               </div>
               <div className="flex items-center h-10">
                 <ThemeSwitcher />
