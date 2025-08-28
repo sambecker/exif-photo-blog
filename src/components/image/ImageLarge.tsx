641ca3accf6546c6ca56fe37b5114f59aa861630
Devin Tyler Cunningham <dtyler.cunningham@gmail.com>
2025-08-28 13:44:31 -0700

    Set default aspect ratio in ImageLarge component

--- src/components/image/ImageLarge.tsx
+++ src/components/image/ImageLarge.tsx
@@ -4,16 +4,17 @@ import ImageWithFallback from './ImageWithFallback';
 
 export default function ImageLarge(props: CustomImageProps) {
   const {
-    aspectRatio,
+    aspectRatio = 1,
     blurCompatibilityMode,
     ...rest
   } = props;
+  const safeAspectRatio = aspectRatio > 0 ? aspectRatio : 1;
   return (
     <ImageWithFallback {...{
       ...rest,
       blurCompatibilityLevel: blurCompatibilityMode ? 'high' : 'none',
       width: IMAGE_WIDTH_LARGE,
-      height: Math.round(IMAGE_WIDTH_LARGE / aspectRatio),
+      height: Math.round(IMAGE_WIDTH_LARGE / safeAspectRatio),
       quality: IMAGE_QUALITY,
     }} />
   );
