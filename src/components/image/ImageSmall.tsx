60b53ece426cd05818cb214359f730114ad65cf8
Devin Tyler Cunningham <dtyler.cunningham@gmail.com>
2025-08-28 13:46:28 -0700

    Update ImageSmall.tsx

--- src/components/image/ImageSmall.tsx
+++ src/components/image/ImageSmall.tsx
@@ -3,16 +3,18 @@ import ImageWithFallback from './ImageWithFallback';
 
 export default function ImageSmall(props: CustomImageProps) {
   const {
-    aspectRatio,
+    aspectRatio = 1,
     blurCompatibilityMode,
     ...rest
   } = props;
+  const safeAspectRatio = aspectRatio > 0 ? aspectRatio : 1;
+
   return (
     <ImageWithFallback {...{
       ...rest,
       blurCompatibilityLevel: blurCompatibilityMode ? 'high' : 'none',
       width: IMAGE_WIDTH_SMALL,
-      height: Math.round(IMAGE_WIDTH_SMALL / aspectRatio),
+      height: Math.round(IMAGE_WIDTH_SMALL / safeAspectRatio),
     }} />
   );
 };
