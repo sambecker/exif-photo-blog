8965dac4ff05e46937f8e09df0ab108684215d8f
Sam Becker <sam@sambecker.com>
2025-09-12 22:11:21 -0500

    Tweak redis client logic

--- src/platforms/redis.ts
+++ src/platforms/redis.ts
@@ -2,17 +2,18 @@ import { Redis } from '@upstash/redis';
 
 const KEY_TEST = 'test';
 
-export const redis =
+export const redis = (
   process.env.KV_URL ||
   process.env.UPSTASH_REDIS_REST_URL
-    ? Redis.fromEnv()
-    : process.env.EXIF_KV_URL &&
-      process.env.EXIF_KV_REST_API_TOKEN
-      ? new Redis({
-        url: process.env.EXIF_KV_URL,
-        token: process.env.EXIF_KV_REST_API_TOKEN,
-      })
-      : undefined;
+) ? Redis.fromEnv()
+  : (
+    process.env.EXIF_KV_URL &&
+    process.env.EXIF_KV_REST_API_TOKEN
+  ) ? new Redis({
+      url: process.env.EXIF_KV_URL,
+      token: process.env.EXIF_KV_REST_API_TOKEN,
+    })
+    : undefined;
 
 export const warmRedisConnection = () => {
   if (redis) { redis.get(KEY_TEST); }
