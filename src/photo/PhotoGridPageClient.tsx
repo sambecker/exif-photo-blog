7ac83dc812845333af615875460a07375e07ace1
Sam Becker <sam@sambecker.com>
2025-09-28 21:38:38 -0500

    Add config for mobile sidebar

--- src/photo/PhotoGridPageClient.tsx
+++ src/photo/PhotoGridPageClient.tsx
@@ -7,10 +7,11 @@ import PhotoGridContainer from './PhotoGridContainer';
 import { ComponentProps, useMemo, useRef, useState } from 'react';
 import clsx from 'clsx/lite';
 import MaskedScroll from '@/components/MaskedScroll';
-import { IS_RECENTS_FIRST } from '@/app/config';
+import { IS_RECENTS_FIRST, SHOW_CATEGORIES_ON_MOBILE } from '@/app/config';
 import { SortBy } from './sort';
 import useViewportHeight from '@/utility/useViewportHeight';
 import PhotoGridSidebarMobile from './PhotoGridSidebarMobile';
+import AnimateItems from '@/components/AnimateItems';
 
 export default function PhotoGridPageClient({
   photos,
@@ -37,37 +38,43 @@ export default function PhotoGridPageClient({
 
   return (
     <div>
-      <div className={clsx(
-        'flex gap-x-2',
-        'md:hidden',
-        'mb-5',
-      )}>
-        {showMobileSidebar
-          ? <PhotoGridSidebar
-            className="grow"
-            {...{
-              ...categories,
-              photosCount: photosCountWithExcludes,
-              containerHeight,
-            }}
-          />
-          : <PhotoGridSidebarMobile
-            className="grow mt-0.5"
-            {...categories}
-          />}
-        <button
-          className={clsx(
-            'self-start',
-            'rounded-lg bg-extra-dim border-medium',
-            'hover:bg-dim active:bg-medium transition-colors',
-            'px-3 pt-[3px] pb-[4px]',
-            'text-sm',
-          )}
-          onClick={() => setShowMobileSidebar(!showMobileSidebar)}
-        >
-          {showMobileSidebar ? 'Less' : 'More'}
-        </button>
-      </div>
+      {SHOW_CATEGORIES_ON_MOBILE &&
+        <AnimateItems
+          animateOnFirstLoadOnly
+          type="bottom"
+          items={[
+            <div key="mobile-sidebar" className={clsx(
+              'flex gap-x-2',
+              'md:hidden',
+              'mb-5',
+            )}>
+              {showMobileSidebar
+                ? <PhotoGridSidebar
+                  className="grow"
+                  {...{
+                    ...categories,
+                    photosCount: photosCountWithExcludes,
+                    containerHeight,
+                  }}
+                />
+                : <PhotoGridSidebarMobile
+                  className="grow mt-0.5"
+                  {...categories}
+                />}
+              <button
+                className={clsx(
+                  'self-start',
+                  'rounded-lg bg-extra-dim border-medium',
+                  'hover:bg-dim active:bg-medium transition-colors',
+                  'px-3 pt-[3px] pb-[4px]',
+                  'text-sm',
+                )}
+                onClick={() => setShowMobileSidebar(!showMobileSidebar)}
+              >
+                {showMobileSidebar ? 'Less' : 'More'}
+              </button>
+            </div>,
+          ]} />}
       <PhotoGridContainer
         cacheKey={`page-${PATH_GRID_INFERRED}`}
         className={clsx(showMobileSidebar && 'max-md:hidden')}
