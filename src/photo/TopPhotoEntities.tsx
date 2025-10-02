c191a002b49f302244c33753c07e8fb0d544d063
Sam Becker <sam@sambecker.com>
2025-10-02 14:28:12 -0500

    Adjust top photo entity content

--- src/photo/TopPhotoEntities.tsx
+++ src/photo/TopPhotoEntities.tsx
@@ -4,9 +4,13 @@ import MaskedScroll from '@/components/MaskedScroll';
 import PhotoAlbum from '@/album/PhotoAlbum';
 import PhotoTag from '@/tag/PhotoTag';
 import PhotoFavs from '@/tag/PhotoFavs';
-import PhotoYear from '@/year/PhotoYear';
 import clsx from 'clsx';
-import { CATEGORY_VISIBILITY } from '@/app/config';
+import {
+  CATEGORY_VISIBILITY,
+  SHOW_ALBUMS,
+  SHOW_RECENTS,
+  SHOW_TAGS,
+} from '@/app/config';
 import PhotoRecents from '@/recents/PhotoRecents';
 import PhotoFilm from '@/film/PhotoFilm';
 import PhotoFocalLength from '@/focal/PhotoFocalLength';
@@ -19,6 +23,9 @@ import { ComponentProps } from 'react';
 import EntityLink from '@/components/entity/EntityLink';
 import { LuPlus } from 'react-icons/lu';
 
+const MAX_ALBUMS = 3;
+const MAX_TAGS = 3;
+
 const ENTITY_LINK_PROPS: Partial<ComponentProps<typeof EntityLink>> = {
   badged: true,
   badgeType: 'medium',
@@ -29,7 +36,6 @@ const ENTITY_LINK_PROPS: Partial<ComponentProps<typeof EntityLink>> = {
 export default function TopPhotoEntities({
   className,
   recents,
-  years,
   cameras,
   lenses,
   albums,
@@ -58,65 +64,66 @@ export default function TopPhotoEntities({
           {...ENTITY_LINK_PROPS}
           badgeIconFirst
         />}
-      {CATEGORY_VISIBILITY.map(category => {
-        switch (category) {
-          case 'recents': return recents.length > 0 &&
-            <PhotoRecents
-              key="recents"
-              {...ENTITY_LINK_PROPS}
-            />;
-          case 'years': return years.length > 0 &&
-            <PhotoYear
-              key="years"
-              year={years[0].year}
-              {...ENTITY_LINK_PROPS}
-            />;
-          case 'cameras': return cameras.length > 0 &&
-            <PhotoCamera
-              key="cameras"
-              camera={cameras[0].camera}
-              {...ENTITY_LINK_PROPS}
-            />;
-          case 'lenses': return lenses.length > 0 &&
-            <PhotoLens
-              key="lenses"
-              lens={lenses[0].lens}
-              {...ENTITY_LINK_PROPS}
-            />;
-          case 'albums': return albums.length > 0 &&
-            <PhotoAlbum
-              key="albums"
-              album={albums[0].album}
-              {...ENTITY_LINK_PROPS}
-            />;
-          case 'tags': return getTopNonFavTags(tags)
-            .map(({ tag })=>
-              <PhotoTag
-                key={tag}
-                tag={tag}
+      {SHOW_RECENTS && recents.length > 0 &&
+        <PhotoRecents
+          key="recents"
+          {...ENTITY_LINK_PROPS}
+        />}
+      {SHOW_ALBUMS && albums.slice(0, MAX_ALBUMS).map(({ album }) =>
+        <PhotoAlbum
+          key={album.id}
+          album={album}
+          {...ENTITY_LINK_PROPS}
+        />,
+      )}
+      {SHOW_TAGS && getTopNonFavTags(tags).slice(0, MAX_TAGS).map(({ tag }) =>
+        <PhotoTag
+          key={tag}
+          tag={tag}
+          {...ENTITY_LINK_PROPS}
+        />,
+      )}
+      {CATEGORY_VISIBILITY
+        .filter(category =>
+          category !== 'recents' &&
+          category !== 'years' &&
+          category !== 'albums' &&
+          category !== 'tags',
+        )
+        .map(category => {
+          switch (category) {
+            case 'cameras': return cameras.length > 0 &&
+              <PhotoCamera
+                key="cameras"
+                camera={cameras[0].camera}
                 {...ENTITY_LINK_PROPS}
-              />,
-            );
-          case 'recipes': return recipes.length > 0 &&
-            <PhotoRecipe
-              key="recipes"
-              recipe={recipes[0].recipe}
-              {...ENTITY_LINK_PROPS}
-            />;
-          case 'films': return films.length > 0 &&
-            <PhotoFilm
-              key="films"
-              film={films[0].film}
-              {...ENTITY_LINK_PROPS}
-            />;
-          case 'focal-lengths': return focalLengths.length > 0 &&
-            <PhotoFocalLength
-              key="focal-lengths"
-              focal={focalLengths[0].focal}
-              {...ENTITY_LINK_PROPS}
-            />;
-        }
-      })}
+              />;
+            case 'lenses': return lenses.length > 0 &&
+              <PhotoLens
+                key="lenses"
+                lens={lenses[0].lens}
+                {...ENTITY_LINK_PROPS}
+              />;
+            case 'recipes': return recipes.length > 0 &&
+              <PhotoRecipe
+                key="recipes"
+                recipe={recipes[0].recipe}
+                {...ENTITY_LINK_PROPS}
+              />;
+            case 'films': return films.length > 0 &&
+              <PhotoFilm
+                key="films"
+                film={films[0].film}
+                {...ENTITY_LINK_PROPS}
+              />;
+            case 'focal-lengths': return focalLengths.length > 0 &&
+              <PhotoFocalLength
+                key="focal-lengths"
+                focal={focalLengths[0].focal}
+                {...ENTITY_LINK_PROPS}
+              />;
+          }
+        })}
       <LoaderButton
         icon={<LuPlus className="text-[0.95rem] translate-y-[1px]" />}
         onClick={() => setIsCommandKOpen?.(true)}
