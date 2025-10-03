b68dca9abe8fe72c5161aa8cd7ef0a81263547c4
Sam Becker <sam@sambecker.com>
2025-10-02 20:56:03 -0500

    Safely access category data

--- src/category/mobile.ts
+++ src/category/mobile.ts
@@ -28,11 +28,11 @@ export const getTopEntities = ({
   hasRecents: SHOW_RECENTS && recents.length > 0,
   albums: SHOW_ALBUMS ? albums.slice(0, MAX_ALBUM_TAG_COUNT) : [],
   tags: SHOW_TAGS ? getTopNonFavTags(tags).slice(0, MAX_ALBUM_TAG_COUNT) : [],
-  recipe: SHOW_RECIPES ? recipes[0].recipe : undefined,
-  film: SHOW_FILMS ? films[0].film : undefined,
-  focal: SHOW_FOCAL_LENGTHS ? focalLengths[0].focal : undefined,
-  camera: SHOW_CAMERAS ? cameras[0].camera : undefined,
-  lens: SHOW_LENSES ? lenses[0].lens : undefined,
+  recipe: SHOW_RECIPES ? recipes[0]?.recipe : undefined,
+  film: SHOW_FILMS ? films[0]?.film : undefined,
+  focal: SHOW_FOCAL_LENGTHS ? focalLengths[0]?.focal : undefined,
+  camera: SHOW_CAMERAS ? cameras[0]?.camera : undefined,
+  lens: SHOW_LENSES ? lenses[0]?.lens : undefined,
 });
 
 export const hasEnoughTopEntities = (categories: PhotoSetCategories) => {
