df2e46b151df7c0d297213cdbc2f7a6a459e97b4
Sam Becker <sam@sambecker.com>
2025-11-25 17:51:41 -0600

    Test caching album images

--- app/album/[album]/image/route.tsx
+++ app/album/[album]/image/route.tsx
@@ -1,40 +1,35 @@
-import { getPhotosCached } from '@/photo/cache';
 import {
   IMAGE_OG_DIMENSION_SMALL,
   MAX_PHOTOS_TO_SHOW_PER_CATEGORY,
 } from '@/image-response';
 import { getIBMPlexMono } from '@/app/font';
 import { ImageResponse } from 'next/og';
-import { getImageResponseCacheControlHeaders } from '@/image-response/cache';
 import { staticallyGenerateCategoryIfConfigured } from '@/app/static';
 import AlbumImageResponse from '@/album/AlbumImageResponse';
 import { getAlbumFromSlug, getAlbumsWithMeta } from '@/album/query';
+import { KEY_PHOTOS } from '@/cache';
+import { cacheTag } from 'next/cache';
+import { getPhotos } from '@/photo/query';
+import { Album } from '@/album';
 
-export const generateStaticParams = staticallyGenerateCategoryIfConfigured(
-  'albums',
-  'image',
-  getAlbumsWithMeta,
-  albums => albums.map(({ album }) => ({ album: album.slug })),
-);
+export const generateStaticParams = async () =>
+  staticallyGenerateCategoryIfConfigured(
+    'albums',
+    'image',
+    getAlbumsWithMeta,
+    albums => albums.map(({ album }) => ({ album: album.slug })),
+  );
 
-export async function GET(
-  _: Request,
-  context: { params: Promise<{ album: string }> },
-) {
-  const { album: albumParam } = await context.params;
-
-  const album = await getAlbumFromSlug(decodeURIComponent(albumParam));
-
-  if (!album) { return new Response('Album not found', { status: 404 }); }
+async function getCacheComponent(album: Album) {
+  'use cache';
+  cacheTag(KEY_PHOTOS);
 
   const [
     photos,
     { fontFamily, fonts },
-    headers,
   ] = await Promise.all([
-    getPhotosCached({ limit: MAX_PHOTOS_TO_SHOW_PER_CATEGORY, album }),
+    getPhotos({ limit: MAX_PHOTOS_TO_SHOW_PER_CATEGORY, album }),
     getIBMPlexMono(),
-    getImageResponseCacheControlHeaders(),
   ]);
 
   const { width, height } = IMAGE_OG_DIMENSION_SMALL;
@@ -47,6 +42,19 @@ export async function GET(
       height,
       fontFamily,
     }}/>,
-    { width, height, fonts, headers },
+    { width, height, fonts },
   );
+}
+
+export async function GET(
+  _: Request,
+  context: { params: Promise<{ album: string }> },
+) {
+  const { album: albumParam } = await context.params;
+
+  const album = await getAlbumFromSlug(decodeURIComponent(albumParam));
+
+  if (!album) { return new Response('Album not found', { status: 404 }); }
+
+  return getCacheComponent(album);
 }
