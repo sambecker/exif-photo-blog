1081ea738c66375d6ca8a4546087c0ed9b2b9938
Sam Becker <sam@sambecker.com>
2026-02-24 17:01:27 -0600

    Add jest TextEncoder polyfill

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
-/* eslint-disable max-len */
 import type { Config } from 'jest';
 import nextJest from 'next/jest.js';
  
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  // Provide the path to your Next.js app to load next.config.js and 
+  // .env files in your test environment
   dir: './',
 });
  
@@ -14,5 +14,6 @@ const config: Config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
  
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+// createJestConfig is exported this way to ensure that
+// next/jest can load the Next.js config which is async
 export default createJestConfig(config);
