89325f6f393dabd1566b4ce4c4e2d5e5ae5bdba1
Sam Becker <sam@sambecker.com>
2026-02-25 18:34:34 -0600

    Make /about/edit safe for blank templates

--- app/about/edit/page.tsx
+++ app/about/edit/page.tsx
@@ -4,9 +4,11 @@ import { PRESERVE_ORIGINAL_UPLOADS } from '@/app/config';
 import { getPhotoNoStore } from '@/photo/cache';
 
 export default async function AboutEditPage() {
-  const about = await getAbout();
+  const about = await getAbout().catch(() => undefined);
+
   const photoAvatar = about?.photoIdAvatar
     ? await getPhotoNoStore(about?.photoIdAvatar ?? '', true)
+      .catch(() => undefined)
     : undefined;
 
   return (
