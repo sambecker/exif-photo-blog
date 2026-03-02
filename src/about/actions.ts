514be4de4a51bbab58766c097c1414214f335284
Sam Becker <sam@sambecker.com>
2026-03-02 08:57:20 -0600

    Fix about edit caching issue

--- src/about/actions.ts
+++ src/about/actions.ts
@@ -4,7 +4,7 @@ import { revalidateAboutKey } from '@/cache';
 import { upsertAbout } from './query';
 import { runAuthenticatedAdminServerAction } from '@/auth/server';
 import { redirect } from 'next/navigation';
-import { PATH_ABOUT } from '@/app/path';
+import { PATH_ABOUT, PATH_ADMIN_ABOUT_EDIT } from '@/app/path';
 import { convertFormDataToAbout } from './form';
 import { revalidatePath } from 'next/cache';
 
@@ -14,5 +14,6 @@ export const updateAboutAction = async (formData: FormData) =>
     await upsertAbout(about);
     revalidateAboutKey();
     revalidatePath(PATH_ABOUT);
+    revalidatePath(PATH_ADMIN_ABOUT_EDIT);
     redirect(PATH_ABOUT);
   });
