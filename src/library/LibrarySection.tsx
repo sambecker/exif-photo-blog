b3c112431f0747b0bf30e759bff673ad13068290
Sam Becker <sam@sambecker.com>
2026-09-14 08:54:33 -0500

    Add icons to film folders

--- src/library/LibrarySection.tsx
+++ src/library/LibrarySection.tsx
@@ -6,24 +6,13 @@ import type { CategoryKey } from '@/category';
 import PhotoFolder from '@/components/folder/PhotoFolder';
 import IconFavs from '@/components/icons/IconFavs';
 import IconRecents from '@/components/icons/IconRecents';
+import PhotoFilmIcon from '@/film/PhotoFilmIcon';
+import { isStringFujifilmSimulation } from '@/platforms/fujifilm/simulation';
 import { TAG_FAVS } from '@/tag';
 import clsx from 'clsx/lite';
 import { ReactNode, useState } from 'react';
 import { LuChevronRight } from 'react-icons/lu';
 import type { LibrarySetFolder } from '.';
-
-const getFolderCaptionIcon = (key: string): ReactNode => {
-  switch (key) {
-    case TAG_FAVS:
-      return <IconFavs
-        size={10}
-        className="translate-y-[-0.5px]"
-        highlight
-      />;
-    case 'recents':
-      return <IconRecents size={10} solid />;
-  }
-};
 
 export default function LibrarySection({
   category,
@@ -36,6 +25,28 @@ export default function LibrarySection({
 }) {
   const [isOpen, setIsOpen] = useState(true);
   const contentId = `library-section-${category}`;
+  const showFilmCaptionIcons = category === 'films' &&
+    folders.some(({ key }) => isStringFujifilmSimulation(key));
+
+  const getFolderCaptionIcon = (key: string): ReactNode => {
+    switch (key) {
+      case TAG_FAVS:
+        return <IconFavs
+          size={10}
+          className="translate-y-[-0.5px]"
+          highlight
+        />;
+      case 'recents':
+        return <IconRecents size={10} solid />;
+    }
+    if (showFilmCaptionIcons) {
+      return <PhotoFilmIcon
+        film={key}
+        height={13}
+        className="translate-y-[-0.5px]"
+      />;
+    }
+  };
 
   return (
     <div className="group/section border-t border-medium pt-1.5 space-y-4">
